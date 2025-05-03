f1ef24d27808fd708cbc21e6f20476149d8b14b8
kije Enki <90457989+kijeEnki@users.noreply.github.com>
2025-05-04 00:37:38 +0300

    Update main.py
    did not change the code in any way but it is more readable and not as dense now

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def prepare():
     table = {}
     with open("korn.csv") as file:
         file = csv.reader(file)
-        for line in file: table[line[0]] = ((line[2], line[4]), (line[1], line[3]))
+        for line in file:
+            table[line[0]] = ((line[2], line[4]), (line[1], line[3]))
 
 def korn(text: str, ipa: bool = False):
     try: table; punctuation; final
@@ -17,16 +18,25 @@ def korn(text: str, ipa: bool = False):
     for mark in punctuation: text = text.replace(mark, "")
     ending = ""
     for mark in final:
-        if text.endswith(mark): ending = mark; text = text.replace(mark, "")
-    words, index = text.split(), 0
+        if text.endswith(mark):
+            ending = mark
+            text = text.replace(mark, "")
+    words = text.split()
+    index = 0
     for word in reversed(words):
-        if word in table: sentence.append(table[word][ipa][index%2==0])
+        if word in table:
+            sentence.append(
+                table[word][ipa][index%2==0]
+            )
         else:
-            if ipa: sentence.append(" ??? ")
-            else: sentence.append(f"“{word}”")
+            if ipa:
+                sentence.append(" ??? ")
+            else:
+                sentence.append(f"“{word}”")
         index += 1
     sentence = list(reversed(sentence))
-    if not ipa: sentence.append(ending)
+    if not ipa:
+        sentence.append(ending)
     return "".join(sentence)
 
 print(korn(original))
